8f60df8d269993c199ea02c1512f765a0490eb4b
mmilenaa <milena.hajieneva@gmail.com>
2026-04-22 16:54:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using EVM_ECLR.Classes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExcelGenerator.CreateExcelFile(
+            filePath: "Merged.xlsx",
+            jitPath: "T_JIT.xlsx",
+            forecastPath: "Forecast.xlsx",
+            totalHoursPath: "Total_Hours.xlsx",
+            routingPath: "Routing_PL.xlsx",
+            backlogPath: "BACKLOG_Export.xlsx"
+        );
+
+        Console.WriteLine("Merge completed!");
+    }
+}
+
